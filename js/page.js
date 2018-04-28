25fb844e5b56e01c56b1122a1236d5ccfd6f8ffa
deronoitz <deronoitz@gmail.com>
2018-04-28 11:35:36 +0700

    cart 1

--- js/page.js
+++ js/page.js
@@ -52,3 +52,23 @@ document.onreadystatechange = function () {
      }, 500)
    }
 }
+
+function getSum(total, num) {
+    return total + num;
+}
+
+var updatePriceCart = function(e){
+  var bp = e.getAttribute("bp");
+  var x = e.value;
+  var count = bp * x;
+  var target = e.parentElement.parentElement.parentElement.querySelector("h3");
+  target.innerHTML = "$"+count+".00"
+
+  var all = document.querySelectorAll('table.cart tbody tr h3');
+  var allVal = [];
+  for (var i = 0; i < all.length - 1; i++) {
+    allVal.push(all[i].textContent.replace( /^\D+/g, '') * 1);
+  }
+  var total = allVal.reduce(getSum);
+  document.querySelector('.cart tr.sum h3').textContent = "$"+total+".00";
+}
